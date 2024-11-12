0de5b2e369360a7e84ec90e64a0eb15e6b445292
Ayush Raj <120264441+aaayu-07@users.noreply.github.com>
2024-11-12 21:35:45 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+	  <hr>
+
+      <footer>
+        <div class="foot_center"><p>Copyright &copy; 2024 LPU laboratory School
+</p>
+		<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbspProgrammed by: Ayush Raj , Himanshu Panda , Ayush Rajput
+</p>
+		</div>
+      </footer>
